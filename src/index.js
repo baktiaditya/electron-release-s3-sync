d6cf94d05aea4d14467fce9fd3eb9acb609774c5
Bakti Aditya <bakti.putra@traveloka.com>
2018-07-20 18:45:33 +0700

    Fix env: node\r

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-const co = require('co')
-const { getLatest } = require('./get-latest')
-const progress = require('request-progress')
-const request = require('request')
-const ProgressBar = require('progress')
-const collect = require('collect-json')
-const nodeUrl = require('url')
+
+const co = require('co');
+const progress = require('request-progress');
+const request = require('request');
+const ProgressBar = require('progress');
+const collect = require('collect-json');
+const nodeUrl = require('url');
+const { getLatest: cb_getLatest } = require('ghreleases');
 
 const {
   GH_USER,
@@ -18,30 +19,40 @@ const {
   AWS_SECRET_KEY,
 } = process.env;
 
-const AWS = require('aws-sdk')
-AWS.config.update({accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_KEY});
-const s3 = require('s3-upload-stream')(new AWS.S3())
+const getLatest = (auth, org, repo, options) => {
+  return new Promise(function(resolve, reject) {
+    const callback = (err, release) => {
+      if (err) reject(err);
+      else resolve(release);
+    };
+    cb_getLatest(auth, org, repo, options || {}, callback);
+  });
+};
+
+const AWS = require('aws-sdk');
+AWS.config.update({ accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_KEY });
+const s3 = require('s3-upload-stream')(new AWS.S3());
 
 const AWS_BUCKET_URL =
   _AWS_BUCKET_URL == null ? null :
     _AWS_BUCKET_URL.endsWith('/') ?
       _AWS_BUCKET_URL :
-      `${AWS_BUCKET_URL}/`
+      `${AWS_BUCKET_URL}/`;
 
 if (GH_USER == null || GH_TOKEN == null) {
-  throw new Error('You must specify both GH_USER and GH_TOKEN in the env.')
+  throw new Error('You must specify both GH_USER and GH_TOKEN in the env.');
 }
 
 if (GH_ORG == null || GH_REPO == null) {
-  throw new Error('You must specify both GH_ORG and GH_REPO in the env.')
+  throw new Error('You must specify both GH_ORG and GH_REPO in the env.');
 }
 
 if (AWS_BUCKET == null || AWS_BUCKET_URL == null) {
-  throw new Error('You must specify the AWS bucket and bucket URL you are uploading to.')
+  throw new Error('You must specify the AWS bucket and bucket URL you are uploading to.');
 }
 
 if (AWS_ACCESS_KEY_ID == null || AWS_SECRET_KEY == null) {
-  throw new Error('You must specify the AWS access key id and secret key.')
+  throw new Error('You must specify the AWS access key id and secret key.');
 }
 
 function sync(options = {}) {
@@ -49,25 +60,25 @@ function sync(options = {}) {
     const { assets } = yield getLatest({
       user: GH_USER,
       token: GH_TOKEN,
-    }, GH_ORG, GH_REPO, options)
+    }, GH_ORG, GH_REPO, options);
 
-    const resources = []
+    const resources = [];
     for (const asset of assets) {
       const {
-        name, url
-      } = asset
-      resources.push({ name, url })
+        name, url,
+      } = asset;
+      resources.push({ name, url });
     }
     resources.sort(({ name: a }, { name: b }) => {
       const _a = a.toUpperCase().includes('LATEST'),
-        _b = b.toUpperCase().includes('LATEST')
-      if (_a == _b) return 0
+        _b = b.toUpperCase().includes('LATEST');
+      if (_a == _b) return 0;
       if (_a && !_b) return 1;
       return -1;
-    })
-    console.log('Identified resources:', resources)
+    });
+    console.log('Identified resources:', resources);
     for (const resource of resources) {
-      const { name, url } = resource
+      const { name, url } = resource;
       const r = progress(request({
         method: 'GET',
         uri: url,
@@ -80,55 +91,55 @@ function sync(options = {}) {
           pass: GH_TOKEN,
           sendImmediately: true,
         },
-      }))
-      const promise_r = new Promise(function (resolve, reject) {
-        let rejected = false
+      }));
+      const promise_r = new Promise(function(resolve, reject) {
+        let rejected = false;
         r.once('error', err => {
-          rejected = true
-          reject(err)
-        })
+          rejected = true;
+          reject(err);
+        });
         r.once('end', () => {
-          if (!rejected) resolve()
-        })
-      })
+          if (!rejected) resolve();
+        });
+      });
       const bar = new ProgressBar(`Uploading ${name} (:size MB) [:bar] :percent eta: :etas (:speed MB/sec)`, {
         complete: '=',
         incomplete: ' ',
         width: 20,
         total: 100,
-        clear: false
-      })
+        clear: false,
+      });
       r.on('progress', state => {
-        const { percent, speed, size: { total: size } } = state
+        const { percent, speed, size: { total: size } } = state;
         bar.update(percent, {
           speed: (speed / 1024 / 1024).toFixed(2),
-          size: (size / 1024 / 1024).toFixed(2)
-        })
-      })
-      r.once('end', () => bar.terminate())
-      let s3_pipe = r
+          size: (size / 1024 / 1024).toFixed(2),
+        });
+      });
+      r.once('end', () => bar.terminate());
+      let s3_pipe = r;
       if (name.toUpperCase().endsWith('JSON')) {
         s3_pipe = s3_pipe.pipe(collect(json => {
-          const file = json.url.substring(json.url.lastIndexOf('/') + 1)
+          const file = json.url.substring(json.url.lastIndexOf('/') + 1);
           json = Object.assign({}, json, {
-            url: nodeUrl.parse(AWS_BUCKET_URL).resolve(file)
-          })
-          return JSON.stringify(json, null, 2)
-        }))
+            url: nodeUrl.parse(AWS_BUCKET_URL).resolve(file),
+          });
+          return JSON.stringify(json, null, 2);
+        }));
       }
       s3_pipe.pipe(s3.upload({
         'Bucket': AWS_BUCKET,
         'Key': name,
-      }))
-      yield promise_r
-      console.log(`Uploaded ${name}.`)
+      }));
+      yield promise_r;
+      console.log(`Uploaded ${name}.`);
     }
-    console.log('Uploaded assets.')
-    return resources
-  })
+    console.log('Uploaded assets.');
+    return resources;
+  });
 }
 
 sync().catch(e => {
-  console.log('err:', e)
-  process.exit(1)
-})
+  console.log('err:', e);
+  process.exit(1);
+});
